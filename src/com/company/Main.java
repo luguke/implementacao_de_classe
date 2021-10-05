805ee751b81b26223a1b9140a8b41ec957e1d632
luguke <lugu.queiroz@hotmail.com>
2021-10-05 15:20:14 -0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Pessoa luis =  new Pessoa();
+        Pessoa giba =  new Pessoa();
+        Pessoa andre =  new Pessoa();
+
+    }
+}
---
+++ src/com/company/pessoa.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Date;
+
+class Pessoa {
+    private String nome;
+    private Date datanascimento;
+    private char genero;
+    private int altura;
+    private String cpf;
+    private String rg;
+    private String etnia;
+    private String nacionalidade;
+    private String cidadeNatal;
+
+}
